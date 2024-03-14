b34f39420552181868c78c65bd54337b1bdd986c
bhuvan243 <bhuvan243@gmail.com>
2024-03-14 11:47:50 +0530

    solution updated

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,48 @@
-
-import React, {useState} from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
-  const [todoList, setTodoList] = useState([{id:1, title:"React"}]);
-  function addTodo() {
-    
+  const [todoList, setTodoList] = useState([{ id: 1, title: "React" }]);
+  function addTodo(event) {
+    event.preventDefault();
+    let lastTodoId = todoList.length === 0 ? 0 : todoList[todoList.length - 1].id
+    setTodoList([...todoList, { id: lastTodoId + 1, title: inputValue }]);
+    setInputValue("");
+  }
+  function deleteTodo(todo) {
+    setTodoList(todoList.filter((item) => item.id!== todo.id));
+
   }
   return (
     <div>
-        {/* Do not remove the main div */}
-        <h2>To-Do List</h2>
-        <div>
-          <input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
-          <button type="submit" onClick={addTodo}>Add Todo</button>
-        </div>
-        <ul>
-          {
-            todoList && (
-              todoList.map(todo => (
-                            <li>
-                              <span>{todo.title}</span>
-                              <button>Delete</button>
-                            </li>)
-                          )
-            )
-          }
-        </ul>
+      {/* Do not remove the main div */}
+      <h2>To-Do List</h2>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          onChange={(e) => setInputValue(e.target.value)}
+          value={inputValue}
+        />
+        <button type="submit" onClick={addTodo}>
+          Add Todo
+        </button>
+      </form>
+      <ul>
+        {todoList &&
+          todoList.map((todo) => (
+            <li>
+              <span>
+                {todo.id}. {todo.title}
+              </span>
+              <button key={todo.id} onClick={() => deleteTodo(todo)}>
+                Delete
+              </button>
+            </li>
+          ))}
+      </ul>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
