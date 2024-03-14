8f9452d0d6545355e22500d99a21f0c1d11a46e8
Bhuvaneshwaran <125134187+bhuvan243@users.noreply.github.com>
2024-03-14 05:58:04 +0000

    ui made

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,33 @@
 
-import React from "react";
+import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [todoList, setTodoList] = useState([{id:1, title:"React"}]);
+  function addTodo() {
+    
+  }
   return (
     <div>
         {/* Do not remove the main div */}
+        <h2>To-Do List</h2>
+        <div>
+          <input type="text" onChange={(e) => setInputValue(e.target.value)} value={inputValue}/>
+          <button type="submit" onClick={addTodo}>Add Todo</button>
+        </div>
+        <ul>
+          {
+            todoList && (
+              todoList.map(todo => (
+                            <li>
+                              <span>{todo.title}</span>
+                              <button>Delete</button>
+                            </li>)
+                          )
+            )
+          }
+        </ul>
     </div>
   )
 }
